c43df48b7e801f5fbe82b3fa6fa7b09f7a8e06d0
LucasAck3rmann <ackermannlucas25@gmail.com>
2023-11-14 14:12:48 -0300

    Updates

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,15 @@
-const taskListContainer = document.querySelector(".app__section-task-list");
+const taskListContainer = document.querySelector('.app__section-task-list')
 
 let tarefas = [
-  {
-    descricao: "Tarefa concluída",
-    concluida: true,
-  },
-  {
-    descricao: "Tarefa pendente",
-    concluida: false,
-  },
-];
+    {
+        descricao: 'Tarefa Concluída',
+        concluida: true
+    },
+    {
+        descricao: 'Tarefa Pendente',
+        concluida: false
+    }
+]
 
 const taskIconSvg = `
 <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -19,27 +19,26 @@ const taskIconSvg = `
         d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
         fill="#01080E" />
 </svg>
-`;
+`
+function createTask(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
 
-function renderTaskList(tarefa) {
-  const taskItem = document.createElement("li");
-  taskItem.classList.add("app__section-task-list-item");
+    const svgIcon = document.createElement('svg')
+    svgIcon.innerHTML = taskIconSvg
 
-  const svgIcon = document.createElement("svg");
-  svgIcon.innerHTML = taskiconSvg;
+    const paragraph = document.createElement('p')
+    paragraph.classList.add('app__section-task-list-item-description')
 
-  const taskDescription = document.createElement("p");
-  taskDescription.classList.add("app__section-task-list-item-description");
+    paragraph.textContent = tarefa.descricao
 
-  taskDescription.textContent = tarefa.descricao;
-
-  taskItem.appendChild(svgIcon);
-  taskItem.appendChild(taskDescription);
-
-  return taskItem;
+    li.appendChild(svgIcon)
+    li.appendChild(paragraph)
+    
+    return li
 }
 
-tarefas.forEach((task) => {
-  const tarefaItem = createTask(task);
-  taskListContainer.appendChild(tarefaItem);
-});
+tarefas.forEach(task => {
+    const taskItem = createTask(task)
+    taskListContainer.appendChild(taskItem)
+})
