0a11b198c8662e61f124af7c6b11f0f20d4b7d48
LucasAck3rmann <ackermannlucas25@gmail.com>
2023-11-14 00:18:45 -0300

    Update

--- script-crud.js
+++ script-crud.js
@@ -11,4 +11,35 @@ let tarefas = [
   },
 ];
 
-const taskiconSvg = 
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`;
+
+function renderTaskList(tarefa) {
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("app__section-task-list-item");
+
+  const svgIcon = document.createElement("svg");
+  svgIcon.innerHTML = taskiconSvg;
+
+  const taskDescription = document.createElement("p");
+  taskDescription.classList.add("app__section-task-list-item-description");
+
+  taskDescription.textContent = tarefa.descricao;
+
+  taskItem.appendChild(svgIcon);
+  taskItem.appendChild(taskDescription);
+
+  return taskItem;
+}
+
+tarefas.forEach((task) => {
+  const tarefaItem = createTask(task);
+  taskListContainer.appendChild(tarefaItem);
+});
