96f345f073322f1e9e687c3eadd2645f9f5c09a1
LucasAck3rmann <ackermannlucas25@gmail.com>
2023-11-13 23:39:47 -0300

    Upload de imagens Svg

--- script-crud.js
+++ script-crud.js
@@ -1,1 +1,14 @@
 const taskListContainer = document.querySelector(".app__section-task-list");
+
+let tarefas = [
+  {
+    descricao: "Tarefa concluída",
+    concluida: true,
+  },
+  {
+    descricao: "Tarefa pendente",
+    concluida: false,
+  },
+];
+
+const taskiconSvg = 
